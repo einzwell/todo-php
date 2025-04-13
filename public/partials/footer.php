3675bade5b26eef307941f9ecf905b6c99a54e09
Einzwell <git@einzwell.dev>
2025-04-13 23:43:19 +0700

    Fix footer Remove .fixed-bottom to avoid obscured page on mobile

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -2,7 +2,7 @@
 
 function footer(): string {
     return <<<HTML
-        <footer class="fixed-bottom bg-body-tertiary text-center">
+        <footer class="bg-body-tertiary text-center">
             <div class="text-center p-3 bg-light-subtle">
                 © 2025 
                 <a href="https://einzwell.dev">Einzwell</a>
